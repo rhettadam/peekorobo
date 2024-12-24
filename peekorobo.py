b28c3b10334ff78f6f84e9a1bd7ccdfa4e833da8
rhettadam <rhettadambusiness@gmail.com>
2024-12-23 20:45:00 -0600

    Debug

--- peekorobo.py
+++ peekorobo.py
@@ -943,4 +943,4 @@ def display_page(pathname, search):
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))  # Get the PORT from Heroku or default to 8050
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
