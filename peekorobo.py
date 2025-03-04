61972d0610ff537410c61a14185e92a7cefa4f1b
rhettadam <rhettadambusiness@gmail.com>
2025-03-04 16:06:26 -0600

    Debug

--- peekorobo.py
+++ peekorobo.py
@@ -2670,4 +2670,4 @@ def display_page(pathname):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))  
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
