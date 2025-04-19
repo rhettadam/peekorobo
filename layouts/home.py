12365c6bd0c1f86c7095ec3e2a0935d497ceb678
rhettadam <rhettadambusiness@gmail.com>
2025-04-19 12:35:03 -0500

    Goodbye

--- layouts/home.py
+++ layouts/home.py
@@ -3,115 +3,21 @@ import dash_bootstrap_components as dbc
 from layouts.topbar import topbar, footer
 
 home_layout = html.Div([
-    topbar,
-    dbc.Container(fluid=True, children=[
+    dbc.Container(
         dbc.Row(
-            [
-                # Left Section: Logo, Text, Search
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.Img(
-                                    src="/assets/logo.png",
-                                    className='homelogo',
-                                    style={
-                                        "width": "400px",
-                                        "marginBottom": "15px",
-                                    },
-                                ),
-                                html.P(
-                                    "Search for any FRC Team",
-                                    style={
-                                        "fontSize": "1.5rem",
-                                        "color": "#555",
-                                        "textAlign": "center",
-                                        "marginBottom": "20px"
-                                    },
-                                ),
-                                dbc.Row(
-                                    [
-                                        dbc.Col(
-                                            dbc.Input(
-                                                id="input-team-home",
-                                                type="text",
-                                                placeholder="Team name or # (e.g., 1912)",
-                                                className="custom-input-box",
-                                                style={"width": "100%", "marginBottom": ".4rem"}
-                                            ),
-                                            width=12
-                                        ),
-                                        dbc.Col(
-                                            dbc.Input(
-                                                id="input-year-home",
-                                                type="text",
-                                                placeholder="Year (e.g., 2025) optional",
-                                                className="custom-input-box",
-                                                style={"width": "100%"}
-                                            ),
-                                            width=12
-                                        ),
-                                    ],
-                                    justify="center",
-                                    style={"marginBottom": "1rem"}
-                                ),
-                                dbc.Button(
-                                    "Search",
-                                    id="btn-search-home",
-                                    color="primary",
-                                    size="lg",
-                                    style={
-                                        "backgroundColor": "#ffdd00ff",
-                                        "border": "2px solid #555",
-                                        "color": "black",
-                                        "marginTop": "10px",
-                                        "width": "50%",
-                                    },
-                                ),
-                            ],
-                            className="logo-search-container",
-                            style={
-                                "textAlign": "center",
-                                "display": "flex",
-                                "flexDirection": "column",
-                                "alignItems": "center"
-                            }
-                        )
-                    ],
-                    width=6,
-                    className="desktop-left-section"
+            dbc.Col(
+                html.H2(
+                    "Sorry, Peekorobo has been discontinued",
+                    style={"textAlign": "center", "color": "#333", "marginTop": "20vh"}
                 ),
-                # Right Section: Bulldozer GIF
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.A(
-                                    html.Img(
-                                        src="/assets/dozer.gif",
-                                        style={
-                                            "width": "100%",  
-                                            "maxWidth": "600px",
-                                            "display": "block",
-                                            "margin": "auto"
-                                        },
-                                        className="dozer-image"
-                                    ),
-                                    href="https://github.com/rhettadam/peekorobo",
-                                    target="_blank",
-                                ),
-                            ],
-                            style={"textAlign": "center"}
-                        )
-                    ],
-                    width=6,
-                    className="desktop-right-section"
-                )
-            ],
+                width=12
+            ),
             justify="center",
             align="center",
             style={"height": "78vh"}
         ),
-    ], class_name="py-5", style={"backgroundColor": "white"}),
-    footer
+        fluid=True,
+        class_name="py-5",
+        style={"backgroundColor": "white"}
+    ),
 ])
